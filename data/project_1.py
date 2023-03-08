39c13aa27fcf428cfc2dc77f98fbf276f7220923
victoriawong1123 <victoriawongc@yahoo.com>
2023-03-08 18:06:47 +0100

    Cleaned up the format

--- data/project_1.py
+++ data/project_1.py
@@ -29,11 +29,11 @@ assets_only = df[df.columns.difference(['DATE', 'WEEKDAY'])]
 assets_byweek = df.loc[df['WEEKDAY'] == 4]
 assets_only_byweek: object = assets_byweek[assets_byweek.columns.difference(['DATE', 'WEEKDAY'])]
 
-"""Diagnostic for individual assets: basic observation"""
+"""1. Diagnostic for individual assets: basic observation"""
 
 
 # Compute daily and weekly simple returns, moving weekly or fridays only
-# 2. Diagnostic for individual assets: basic observation
+# 1. Diagnostic for individual assets: basic observation
 def assets_returns(assets, period, compounded=True):
     if not compounded:
         asset_return = (assets / assets.shift(periods=period)) - 1
@@ -44,19 +44,6 @@ def assets_returns(assets, period, compounded=True):
     asset_return['DATE'] = df['DATE']
     asset_return.set_index('DATE', inplace=True)
     return asset_return
-
-
-# Getting daily/weekly simple and compounded returns
-# 2a. Compare simple return and log-returns in daily frequency.
-daily_simple = assets_returns(assets_only, 1, False)
-weekly_simple = assets_returns(assets_only_byweek, 1, False)
-daily_compounded = assets_returns(assets_only, 1, True)
-weekly_compounded = assets_returns(assets_only_byweek, 1, True)
-
-# Compare the differences between log and simple returns
-# 2a
-daily_diff = daily_simple - daily_compounded
-weekly_diff = weekly_simple - weekly_compounded
 
 
 # Graphs for showing the differences between simple and compounded returns
@@ -84,19 +71,11 @@ def log_vs_simple(x, y, series1, series2, title):
     plt.show()
 
 
-# Graphs for comparing the differences between simple and compounded returns
-log_vs_simple(3, 2, daily_simple, daily_compounded, 'Daily')
-log_vs_simple(3, 2, weekly_simple, weekly_compounded, 'Weekly')
-
 """1c. How are the above descriptive statistics changed when you change the 
 frequency from daily to weekly? """
 
-# Descriptive Statistics of log return
-daily_compounded.describe()
-weekly_compounded.describe()
 
-
-# 2c. Computing the mean, volatility, skewness and kurtosis of compounded returns
+# 1c. Computing the mean, volatility, skewness and kurtosis of compounded returns
 def return_moments(series: object, moments: list) -> pd.DataFrame:
     des_summary = pd.DataFrame()
     for moment in moments:
@@ -111,26 +90,26 @@ def return_moments(series: object, moments: list) -> pd.DataFrame:
     return des_summary.transpose()
 
 
-# Generating the mean, volatility, skewness and kurtosis of daily and weekly compounded returns
-daily_res_compounded = return_moments(daily_compounded, [1, 2, 3, 4])
-weekly_res_compounded = return_moments(weekly_compounded, [1, 2, 3, 4])
-daily_res_simple = return_moments(daily_simple, [1, 2, 3, 4])
-weekly_res_simple = return_moments(weekly_simple, [1, 2, 3, 4])
-
-
-def convert_annualized(ds):
+def convert_annualized(ds, frequency):
     ds = ds.copy()
-    ds.loc['An. Mean'] = (((1 + ds.loc['Mean']) ** 252 - 1) * 100)
-    ds.loc['An. Std. Dev'] = (ds.loc['Std. Dev']*np.sqrt(252) * 100)
+    if frequency == 'd':
+        ds.loc['An. Mean'] = (((1 + ds.loc['Mean']) ** 252 - 1) * 100)
+        ds.loc['An. Std. Dev'] = (ds.loc['Std. Dev'] * np.sqrt(252) * 100)
+    elif frequency == 'w':
+        ds.loc['An. Mean'] = (((1 + ds.loc['Mean']) ** 52 - 1) * 100)
+        ds.loc['An. Std. Dev'] = (ds.loc['Std. Dev'] * np.sqrt(52) * 100)
+    elif frequency == 'm':
+        ds.loc['An. Mean'] = (((1 + ds.loc['Mean']) ** 12 - 1) * 100)
+        ds.loc['An. Std. Dev'] = (ds.loc['Std. Dev'] * np.sqrt(12) * 100)
     ds.loc['Mean'] = (ds.loc['Mean'] * 100)
     ds.loc['Std. Dev'] = (ds.loc['Std. Dev'] * 100)
     return ds.round(4)
 
 
-"""3. Diagnostic for individual assets: exploration"""
+"""2. Diagnostic for individual assets: exploration"""
 
 
-# 3a. Find the maximum and minimum returns of the S&P500
+# 2a. Find the maximum and minimum returns of the S&P500
 def get_max(series, col_name: str, i):
     max_returns = series.nlargest(i, col_name, keep='first')
     col_max = max_returns[col_name]
@@ -143,46 +122,7 @@ def get_min(series, col_name: str, i):
     return col_min
 
 
-stock_max = get_max(daily_compounded, 'SP500', 5)
-stock_min = get_min(daily_compounded, 'SP500', 5)
-# def get_max_min(series, col_name):
-
-
-# def plot_max_min()
-fig, ax = plt.subplots(figsize=(5, 2.3), dpi=300)
-# ax.plot(daily_compounded['SP500'], ls='-', color='b')
-ax.plot(df.DATE, df['SP500'], color='b')
-ax.spines['left'].set_color('b')
-ax.yaxis.label.set_color('b')
-ax.tick_params(axis='y', colors='b')
-ax.set_ylim(0, 15000)
-ax.set_xlabel('Year')
-ax.set_ylabel('Price')
-ax.set_title('SP500')
-
-axt = ax.twinx()
-axt.plot(daily_compounded.index, daily_compounded.SP500, color='r', alpha=0.6)
-axt.spines['right'].set_color('r')
-axt.yaxis.label.set_color('r')
-axt.tick_params(axis='y', colors='r')
-axt.set_ylim(-0.4, 0.2)
-axt.set_ylabel('Return')
-# axt.axhline(y=0, color='k')
-for d in stock_max.index.values:
-    axt.axvline(d, color='k', ls='--', lw=0.7)
-for d in stock_min.index.values:
-    axt.axvline(d, color='g', ls='-', lw=0.7)
-
-axt.plot([], [], color='k', ls='--', lw=0.7, label='max')
-axt.plot([], [], color='g', ls='-', lw=0.7, label='min')
-axt.legend(loc=3, frameon=False, bbox_to_anchor=(0, 0.15))
-
-fig.tight_layout()
-
-plt.show()
-
-
-# 3b Test if the magnitude of the crashes and booms is consistent with the hypothesis of normality
+# 2b Test if the magnitude of the crashes and booms is consistent with the hypothesis of normality
 def cal_pvalue(df: pd.DataFrame) -> pd.DataFrame:
     out = {}
     for k in df.keys():
@@ -207,17 +147,6 @@ def jb_statistics(returns, skewness, kurtosis):
     return pd.DataFrame(jb_score, index=['Jarque Bera Score'])
 
 
-# Getting the daily and weekly skewness and kurtosis of all assets
-daily_skewness = pd.DataFrame(daily_res_compounded.loc['Skewness', :]).transpose()
-daily_kurtosis = pd.DataFrame(daily_res_compounded.loc['Kurtosis', :]).transpose()
-weekly_skewness = pd.DataFrame(weekly_res_compounded.loc['Skewness', :]).transpose()
-weekly_kurtosis = pd.DataFrame(weekly_res_compounded.loc['Kurtosis', :]).transpose()
-
-# Generating the Jarque Bera test statistics of all assets
-jarque_scores_daily = jb_statistics(daily_compounded, daily_skewness, daily_kurtosis)
-jarque_scores_weekly = jb_statistics(weekly_compounded, weekly_skewness, weekly_kurtosis)
-
-
 # Testing normality
 # H0: Skewness and excess kurtosis are independent of each other -> normality
 # H1: Skewness and excess kurtosis are dependent of each other -> non-normal
@@ -228,7 +157,7 @@ def jb_test(jb_stats, dof, alpha):
             print(f'Reject H0')
 
 
-"""4. Diagnostic for a portfolio"""
+"""3. Diagnostic for a portfolio"""
 """Portfolio using an equally weighted allocation, 
 we are computing the portfolio return as the average of the daily (weekly) 
 simple return of the six asset classes """
@@ -247,15 +176,81 @@ def portfolio_return(weight, daily):
     return daily_portfolio_return
 
 
-asset_weight = equal_weight(assets_only)
-daily_portfolio = portfolio_return(asset_weight, daily_simple)
-weekly_portfolio = portfolio_return(asset_weight, weekly_simple)
-
-# 4a. Descriptive statistics of portfolio daily returns
-daily_portfolio_char = return_moments(daily_portfolio, moments=[1, 2, 3, 4])
-weekly_portfolio_char = return_moments(weekly_portfolio, moments=[1, 2, 3, 4])
-
 if __name__ == '__main__':
+    # Getting daily/weekly simple and compounded returns
+    # 1a. Compare simple return and log-returns in daily frequency.
+    daily_simple = assets_returns(assets_only, 1, False)
+    weekly_simple = assets_returns(assets_only_byweek, 1, False)
+    daily_compounded = assets_returns(assets_only, 1, True)
+    weekly_compounded = assets_returns(assets_only_byweek, 1, True)
+
+    # Compare the differences between log and simple returns
+    # 1a
+    daily_diff = daily_simple - daily_compounded
+    weekly_diff = weekly_simple - weekly_compounded
+
+    # Graphs for comparing the differences between simple and compounded returns
+    log_vs_simple(3, 2, daily_simple, daily_compounded, 'Daily')
+    log_vs_simple(3, 2, weekly_simple, weekly_compounded, 'Weekly')
+
+    # Descriptive Statistics of log return
+    log_stats_daily = daily_compounded.describe().head(15)
+    log_stats_weekly = weekly_compounded.describe().head(15)
+
+    # Generating the mean, volatility, skewness and kurtosis of daily and weekly compounded returns
+    daily_res_compounded = return_moments(daily_compounded, [1, 2, 3, 4])
+    weekly_res_compounded = return_moments(weekly_compounded, [1, 2, 3, 4])
+    daily_res_simple = return_moments(daily_simple, [1, 2, 3, 4])
+    weekly_res_simple = return_moments(weekly_simple, [1, 2, 3, 4])
+
+    stock_max = get_max(daily_compounded, 'SP500', 5)
+    stock_min = get_min(daily_compounded, 'SP500', 5)
+
+    fig, ax = plt.subplots(figsize=(5, 2.3), dpi=300)
+    # ax.plot(daily_compounded['SP500'], ls='-', color='b')
+    ax.plot(df.DATE, df['SP500'], color='b')
+    ax.spines['left'].set_color('b')
+    ax.yaxis.label.set_color('b')
+    ax.tick_params(axis='y', colors='b')
+    ax.set_ylim(0, 15000)
+    ax.set_xlabel('Year')
+    ax.set_ylabel('Price')
+    ax.set_title('SP500')
+
+    axt = ax.twinx()
+    axt.plot(daily_compounded.index, daily_compounded.SP500, color='r', alpha=0.6)
+    axt.spines['right'].set_color('r')
+    axt.yaxis.label.set_color('r')
+    axt.tick_params(axis='y', colors='r')
+    axt.set_ylim(-0.4, 0.2)
+    axt.set_ylabel('Return')
+    # axt.axhline(y=0, color='k')
+    for d in stock_max.index.values:
+        axt.axvline(d, color='k', ls='--', lw=0.7)
+    for d in stock_min.index.values:
+        axt.axvline(d, color='g', ls='-', lw=0.7)
+
+    axt.plot([], [], color='k', ls='--', lw=0.7, label='max')
+    axt.plot([], [], color='g', ls='-', lw=0.7, label='min')
+    axt.legend(loc=3, frameon=False, bbox_to_anchor=(0, 0.15))
+
+    fig.tight_layout()
+
+    plt.show()
+
+    # 1c. Getting the daily and weekly skewness and kurtosis of all assets
+    daily_skewness = pd.DataFrame(daily_res_compounded.loc['Skewness', :]).transpose()
+    daily_kurtosis = pd.DataFrame(daily_res_compounded.loc['Kurtosis', :]).transpose()
+    weekly_skewness = pd.DataFrame(weekly_res_compounded.loc['Skewness', :]).transpose()
+    weekly_kurtosis = pd.DataFrame(weekly_res_compounded.loc['Kurtosis', :]).transpose()
+
+    # Annualized return and standard deviation
+    daily_simple_an = convert_annualized(daily_res_simple, 'd')
+    daily_com_an = convert_annualized(daily_res_compounded, 'd')
+    weekly_simple_an = convert_annualized(weekly_res_simple, 'w')
+    weekly_com_an = convert_annualized(weekly_res_compounded, 'w')
+
+    # 2b. 3-sigma event
     daily_pvalue = cal_pvalue(daily_compounded)
     weekly_pvalue = cal_pvalue(weekly_compounded)
 
@@ -263,3 +258,25 @@ if __name__ == '__main__':
     weekly_pvalue.index = ['weekly_pvalue']
     pval = pd.concat([daily_pvalue, weekly_pvalue])
     print(pval)
+
+    # 2c. Generating the Jarque Bera test statistics of all assets
+    jarque_scores_daily = jb_statistics(daily_compounded, daily_skewness, daily_kurtosis)
+    jarque_scores_weekly = jb_statistics(weekly_compounded, weekly_skewness, weekly_kurtosis)
+
+    # 3a. Equally weighted portfolio based on simple returns
+    asset_weight = equal_weight(assets_only)
+    daily_portfolio = portfolio_return(asset_weight, daily_res_simple)
+    weekly_portfolio = portfolio_return(asset_weight, weekly_res_simple)
+
+    # 3a. Descriptive statistics of portfolio daily returns
+    daily_portfolio_char = return_moments(daily_portfolio, moments=[1, 2, 3, 4])
+    weekly_portfolio_char = return_moments(weekly_portfolio, moments=[1, 2, 3, 4])
+
+    daily_log_min = daily_res_compounded.min()
+    daily_log_max = daily_res_compounded.max()
+    daily_simple_min = daily_res_simple.min()
+    daily_simple_max = daily_res_simple.max()
+    weekly_log_min = weekly_res_compounded.min()
+    weekly_log_max = weekly_res_compounded.max()
+    weekly_simple_min = weekly_res_simple.min()
+    weekly_simple_max = weekly_res_simple.max()
